5f7b454f16c98859c5aa5b9027bddd2874c3ee06
Dmitry Stelmakhov <jaiz@yandex.ru>
2021-11-10 23:58:27 +0300

    refactoring run_game

--- settings.py
+++ settings.py
@@ -7,4 +7,4 @@ class Settings:
         # Параметры экрана.
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (3, 32, 64)
